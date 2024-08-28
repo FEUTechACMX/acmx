55af80ae42dd480a4ed4c2ba2b65b9aa416ae03a
Alpha Romer Coma <alpharomercoma@proton.me>
2024-08-28 14:42:41 +0000

    refactor: undertaking generator logic

---
+++ src/utils/validImage.ts
@@ -0,0 +1,12 @@
+const extMap = new Map([
+    ["image/png", 1],
+    ["image/jpg", 2],
+    ["image/jpeg", 2],
+]);
+const undertakingImgType = (ext: string) => {
+    const imgType = extMap.get(ext);
+    if (!imgType) throw new Error("Invalid image type");
+    return imgType;
+};
+
+export default undertakingImgType;
