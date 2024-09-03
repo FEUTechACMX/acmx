1d835a43f82a124ed2a9a46fa60cd73a7f6fec0a
Alpha Romer Coma <202211383@fit.edu.ph>
2024-09-03 22:18:09 +0000

    refactor: clean undertaking code & exports

---
+++ src/utils/serverWrapper.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+const serverWrapper = (
+    fn: (
+        req: NextRequest,
+        res: NextResponse,
+    ) => NextResponse | Promise<NextResponse>,
+) => {
+    return async (req: NextRequest, res: NextResponse) => {
+        try {
+            return await fn(req, res);
+        } catch (e) {
+            return new NextResponse(e?.toString(), {
+                status: 422,
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+        }
+    };
+};
+
+export default serverWrapper;
