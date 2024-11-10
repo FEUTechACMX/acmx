acfc63389d0a8f2734ba478de775e28cb84f6a70
Alpha Romer Coma <alpharomercoma@proton.me>
2024-11-10 23:16:48 +0800

    fix: failing build due to bump deps

--- src/app/2024/starting/page.tsx
+++ src/app/2024/starting/page.tsx
@@ -1,10 +1,7 @@
-import Header from "~/components/Home/Header";
-
-
 const Home = () => {
   return (
     <div>
-    
+
     </div>
   )
 }
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,11 +1,8 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "~/lib/utils"
+import { cn } from "~/lib/utils";
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
@@ -22,4 +19,4 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 )
 Input.displayName = "Input"
 
-export { Input }
+export { Input };
