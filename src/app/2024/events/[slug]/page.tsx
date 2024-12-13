bcb997d84921609078125359dd9a263c14d24528
noodles-1 <adrianeroa081202@gmail.com>
2024-12-13 14:25:16 +0800

    feat: design mobile view

--- src/app/2024/events/[slug]/page.tsx
+++ src/app/2024/events/[slug]/page.tsx
@@ -20,17 +20,28 @@ const Event = async ({
     const tags = ["tag 1", "tag 2", "tag 3", "tag 4", "tag 5"];
 
     return (
-        <div className="mb-[20dvh]"> 
+        <div className="relative mb-[8rem]"> 
             <section className="relative">
                 <img src="/placeholder/bg.png" alt="" className="object-cover w-full h-96" />
                 <div className="absolute inset-0 bg-gradient-to-t from-background to-transparent" />
             </section>
-            <section className="relative flex justify-center">
+            <section className="fixed lg:hidden z-50 bottom-0 p-2 w-full">
+                <div className="bg-[#F9F9F9] px-5 py-4 rounded-2xl shadow-[0_0_20px_0_rgba(0,0,0,0.4)]">
+                    <div className="flex justify-between items-end">
+                        <h1 className="font-bold text-[#6A086A] text-2xl"> ₱ 150.00 / Free </h1>
+                        <p className="font-light text-[#474747] text-md"> 14 tickets left </p>
+                    </div>
+                    <div className="mt-2 bg-black inline-block rounded-md w-full">
+                        <Button className="text-md font-bold h-[3rem] w-full bg-gradient-to-r from-[#A330B0] via-[#58138D] to-[#3D017D] hover:opacity-80 transition-all duration-150"> Register now </Button>
+                    </div>
+                </div>
+            </section>
+            <section className="relative flex justify-center mx-10">
                 <main className="relative flex gap-10 -top-6">
-                    <section className="max-w-[50rem]">
+                    <section className="relative max-w-[50rem]">
                         <section>
-                            <h1 className="font-black text-5xl inline-block text-transparent bg-clip-text bg-gradient-to-r from-[#A330B0] via-[#58138D] to-[#3D017D]"> Headline </h1>
-                            <div className="flex flex-wrap w-[30rem] gap-2 mt-2">
+                            <h1 className="font-black text-4xl md:text-5xl inline-block text-transparent bg-clip-text bg-gradient-to-r from-[#A330B0] via-[#58138D] to-[#3D017D]"> Headline </h1>
+                            <div className="flex flex-wrap lg:w-[30rem] gap-2 mt-2">
                                 {tags.map((tag, idx) => (
                                     <Tag key={idx} name={tag} />
                                 ))}
@@ -38,7 +49,26 @@ const Event = async ({
                         </section>
                         <section className="mt-5">
                             <h2 className="font-extrabold text-xl"> Subhead </h2>
-                            <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores labore, libero nulla in ducimus aliquid laboriosam corporis veritatis tenetur corrupti minus quod totam sequi natus cum voluptas nemo ipsum expedita reiciendis explicabo blanditiis possimus provident voluptate. Magni eligendi minus necessitatibus neque ex quia temporibus in cum, voluptatem qui ad perferendis. </p>
+                            <p className="text-justify"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores labore, libero nulla in ducimus aliquid laboriosam corporis veritatis tenetur corrupti minus quod totam sequi natus cum voluptas nemo ipsum expedita reiciendis explicabo blanditiis possimus provident voluptate. Magni eligendi minus necessitatibus neque ex quia temporibus in cum, voluptatem qui ad perferendis. </p>
+                        </section>
+                        <section className="lg:hidden mt-4">
+                            <div className="bg-[#F9F9F9] p-9 rounded-xl shadow-xl">
+                                <h2 className="font-bold"> Venue information </h2>
+                                <div className="flex flex-col gap-3 mt-4">
+                                    <div className="flex items-center gap-4">
+                                        <User stroke="#653765" strokeWidth={2} size={24} />
+                                        <p> FEU Tech ACM Student Chapter </p>
+                                    </div>
+                                    <div className="flex items-center gap-4">
+                                        <MapPin stroke="#653765" strokeWidth={2} size={24} />
+                                        <p> FEU Institute of Technology </p>
+                                    </div>
+                                    <div className="flex items-center gap-4">
+                                        <Calendar1 stroke="#653765" strokeWidth={2} size={24} />
+                                        <p> November 10, 2024 · 12:00 PM </p>
+                                    </div>
+                                </div>
+                            </div>
                         </section>
                         <section className="mt-5">
                             <h2 className="font-extrabold text-xl"> Media </h2>
@@ -49,7 +79,7 @@ const Event = async ({
                                 <CarouselContent className="">
                                     {Array.from({ length: 2}).map((_, idx) => (
                                         <CarouselItem key={idx}>
-                                            <img src="/placeholder/bg.png" alt="" className="object-cover w-full h-[30rem] rounded-xl" />
+                                            <img src="/placeholder/bg.png" alt="" className="object-cover w-full h-[14rem] md:h-[30rem] rounded-xl" />
                                         </CarouselItem>
                                     ))}
                                 </CarouselContent>
@@ -66,7 +96,7 @@ const Event = async ({
                                 <CarouselContent className="">
                                     {Array.from({ length: 5 }).map((_, idx) => (
                                         <CarouselItem key={idx} className="basis-1/3">
-                                            <img src="/placeholder/bg.png" alt="" className="object-cover w-full h-[10rem] rounded-xl" />
+                                            <img src="/placeholder/bg.png" alt="" className="object-cover w-full h-[6rem] md:h-[10rem] rounded-xl" />
                                         </CarouselItem>
                                     ))}
                                 </CarouselContent>
@@ -75,7 +105,7 @@ const Event = async ({
                             </Carousel>
                         </section>
                     </section>
-                    <section className="min-w-[22rem] flex flex-col gap-[0.1rem] sticky top-8 self-start">
+                    <section className="min-w-[22rem] hidden lg:flex flex-col gap-[0.1rem] sticky top-8 self-start">
                         <div className="bg-[#F9F9F9] p-9 rounded-t-xl shadow-xl">
                             <h2 className="font-bold"> Venue information </h2>
                             <div className="flex flex-col gap-3 mt-4">
