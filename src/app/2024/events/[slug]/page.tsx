aeeaa7c9bbb84d30c0f16a2721e558f000d4021a
noodles-1 <adrianeroa081202@gmail.com>
2024-12-13 22:27:54 +0800

    refactor: venue information and register

--- src/app/2024/events/[slug]/page.tsx
+++ src/app/2024/events/[slug]/page.tsx
@@ -26,15 +26,7 @@ const Event = async ({
                 <div className="absolute inset-0 bg-gradient-to-t from-background to-transparent" />
             </section>
             <section className="fixed lg:hidden z-50 bottom-0 p-2 w-full">
-                <div className="bg-[#F9F9F9] dark:bg-[#261a35] px-5 py-4 rounded-2xl shadow-[0_0_20px_0_rgba(0,0,0,0.4)] dark:shadow-[0_0_30px_0_rgba(0,0,0,0.8)]">
-                    <div className="flex justify-between items-end">
-                        <h1 className="font-bold text-[#6A086A] dark:text-[#E4CAFF] text-2xl"> ₱ 150.00 / Free </h1>
-                        <p className="font-light text-[#474747] dark:text-[#CFCFCF] text-md"> 14 tickets left </p>
-                    </div>
-                    <div className="mt-2 bg-black inline-block rounded-md w-full">
-                        <Button className="text-md font-bold h-[3rem] sm:h-[3.5rem] w-full bg-gradient-to-r from-[#A330B0] via-[#58138D] to-[#3D017D] dark:from-[#C676C6] dark:via-[#AC79D0] dark:to-[#A261E5] hover:opacity-80 transition-all duration-150"> Register now </Button>
-                    </div>
-                </div>
+                <Register mobileView />
             </section>
             <section className="relative flex justify-center mx-6 md:mx-10">
                 <main className="relative flex gap-10 -top-6">
@@ -52,23 +44,7 @@ const Event = async ({
                             <p className="text-justify"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores labore, libero nulla in ducimus aliquid laboriosam corporis veritatis tenetur corrupti minus quod totam sequi natus cum voluptas nemo ipsum expedita reiciendis explicabo blanditiis possimus provident voluptate. Magni eligendi minus necessitatibus neque ex quia temporibus in cum, voluptatem qui ad perferendis. </p>
                         </section>
                         <section className="lg:hidden mt-4">
-                            <div className="bg-[#F9F9F9] dark:bg-[#261a35] p-7 md:p-9 rounded-xl shadow-xl">
-                                <h2 className="font-bold"> Venue information </h2>
-                                <div className="flex flex-col gap-3 mt-4">
-                                    <div className="flex items-center gap-4">
-                                        <User stroke="#653765" strokeWidth={2} size={24} />
-                                        <p> FEU Tech ACM Student Chapter </p>
-                                    </div>
-                                    <div className="flex items-center gap-4">
-                                        <MapPin stroke="#653765" strokeWidth={2} size={24} />
-                                        <p> FEU Institute of Technology </p>
-                                    </div>
-                                    <div className="flex items-center gap-4">
-                                        <Calendar1 stroke="#653765" strokeWidth={2} size={24} />
-                                        <p> November 10, 2024 · 12:00 PM </p>
-                                    </div>
-                                </div>
-                            </div>
+                            <VenueInformation mobileView />
                         </section>
                         <section className="mt-5">
                             <h2 className="font-extrabold text-xl"> Media </h2>
@@ -106,33 +82,8 @@ const Event = async ({
                         </section>
                     </section>
                     <section className="min-w-[22rem] hidden lg:flex flex-col gap-[0.1rem] sticky top-8 self-start">
-                        <div className="bg-[#F9F9F9] dark:bg-[#261a35] p-9 rounded-t-xl shadow-xl">
-                            <h2 className="font-bold"> Venue information </h2>
-                            <div className="flex flex-col gap-3 mt-4">
-                                <div className="flex items-center gap-4">
-                                    <User stroke="#653765" strokeWidth={2} size={24} className="dark:hidden" />
-                                    <User stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block" />
-                                    <p> FEU Tech ACM Student Chapter </p>
-                                </div>
-                                <div className="flex items-center gap-4">
-                                    <MapPin stroke="#653765" strokeWidth={2} size={24} className="dark:hidden" />
-                                    <MapPin stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block" />
-                                    <p> FEU Institute of Technology </p>
-                                </div>
-                                <div className="flex items-center gap-4">
-                                    <Calendar1 stroke="#653765" strokeWidth={2} size={24} className="dark:hidden"/>
-                                    <Calendar1 stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block"/>
-                                    <p> November 10, 2024 · 12:00 PM </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="bg-[#F9F9F9] dark:bg-[#261a35] p-9 rounded-b-xl shadow-xl">
-                            <h1 className="font-extrabold text-[#6A086A] dark:text-[#E4CAFF] text-3xl"> ₱ 150.00 / Free </h1>
-                            <p className="font-light text-[#474747] dark:text-[#CFCFCF] text-md"> 14 tickets left </p>
-                            <div className="mt-2 bg-black inline-block rounded-md">
-                                <Button className="text-lg font-semibold h-[3rem] w-[10rem] bg-gradient-to-r from-[#A330B0] via-[#58138D] to-[#3D017D] dark:from-[#C676C6] dark:via-[#AC79D0] dark:to-[#A261E5] hover:opacity-80 transition-all duration-150"> Register now </Button>
-                            </div>
-                        </div>
+                        <VenueInformation />
+                        <Register />
                     </section>
                 </main>
             </section>
@@ -145,6 +96,82 @@ const Tag = ({ name }: { name: string }) => {
         <span className="bg-[#F9F9F9] dark:bg-[#261a35] px-5 py-[0.4rem] rounded-2xl text-center shadow-lg text-sm">
             {name}
         </span>
+    );
+}
+
+const Register = ({ mobileView }: { mobileView?: boolean }) => {
+    if (mobileView)
+        return (
+            <div className="bg-[#F9F9F9] dark:bg-[#261a35] px-5 py-4 rounded-2xl shadow-[0_0_20px_0_rgba(0,0,0,0.4)] dark:shadow-[0_0_30px_0_rgba(0,0,0,0.8)]">
+                <div className="flex justify-between items-end">
+                    <h1 className="font-bold text-[#6A086A] dark:text-[#E4CAFF] text-xl sm:text-2xl"> ₱ 150.00 / Free </h1>
+                    <p className="font-light text-[#474747] dark:text-[#CFCFCF] text-md"> 14 tickets left </p>
+                </div>
+                <div className="mt-2 bg-black inline-block rounded-md w-full">
+                    <Button className="text-md font-bold h-[3rem] sm:h-[3.5rem] w-full bg-gradient-to-r from-[#A330B0] via-[#58138D] to-[#3D017D] dark:from-[#C676C6] dark:via-[#AC79D0] dark:to-[#A261E5] hover:opacity-80 transition-all duration-150"> Register now </Button>
+                </div>
+            </div>
+        );
+    
+    return (
+        <div className="bg-[#F9F9F9] dark:bg-[#261a35] p-9 rounded-b-xl shadow-xl">
+            <h1 className="font-extrabold text-[#6A086A] dark:text-[#E4CAFF] text-3xl"> ₱ 150.00 / Free </h1>
+            <p className="font-light text-[#474747] dark:text-[#CFCFCF] text-md"> 14 tickets left </p>
+            <div className="mt-2 bg-black inline-block rounded-md">
+                <Button className="text-lg font-semibold h-[3rem] w-[10rem] bg-gradient-to-r from-[#A330B0] via-[#58138D] to-[#3D017D] dark:from-[#C676C6] dark:via-[#AC79D0] dark:to-[#A261E5] hover:opacity-80 transition-all duration-150"> Register now </Button>
+            </div>
+        </div>
+    );
+}
+
+const VenueInformation = ({ mobileView }: { mobileView?: boolean }) => {
+    if (mobileView)
+        return (
+            <div className="bg-[#F9F9F9] dark:bg-[#261a35] p-7 md:p-9 rounded-xl shadow-xl">
+                <h2 className="font-bold"> Venue information </h2>
+                <div className="flex flex-col gap-3 mt-4">
+                    <div className="flex items-center gap-4">
+                        <User stroke="#653765" strokeWidth={2} size={24} className="dark:hidden" />
+                        <User stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block" />
+                        <p> FEU Tech ACM Student Chapter </p>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <MapPin stroke="#653765" strokeWidth={2} size={24} className="dark:hidden" />
+                        <MapPin stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block" />
+                        <p> FEU Institute of Technology </p>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <Calendar1 stroke="#653765" strokeWidth={2} size={24} className="dark:hidden"/>
+                        <Calendar1 stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block"/>
+                        <p> November 10, 2024 · 12:00 PM </p>
+                    </div>
+                </div>
+            </div>
+        );
+
+    return (
+        <>
+            <div className="bg-[#F9F9F9] dark:bg-[#261a35] p-9 rounded-t-xl shadow-xl">
+                <h2 className="font-bold"> Venue information </h2>
+                <div className="flex flex-col gap-3 mt-4">
+                    <div className="flex items-center gap-4">
+                        <User stroke="#653765" strokeWidth={2} size={24} className="dark:hidden" />
+                        <User stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block" />
+                        <p> FEU Tech ACM Student Chapter </p>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <MapPin stroke="#653765" strokeWidth={2} size={24} className="dark:hidden" />
+                        <MapPin stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block" />
+                        <p> FEU Institute of Technology </p>
+                    </div>
+                    <div className="flex items-center gap-4">
+                        <Calendar1 stroke="#653765" strokeWidth={2} size={24} className="dark:hidden"/>
+                        <Calendar1 stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block"/>
+                        <p> November 10, 2024 · 12:00 PM </p>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
  
