5a0eb2fbdec640ae8dc7a3f03f83a46f3698f3e9
noodles-1 <adrianeroa081202@gmail.com>
2024-12-14 10:25:33 +0800

    feat: events backend

--- src/app/2024/events/[slug]/page.tsx
+++ src/app/2024/events/[slug]/page.tsx
@@ -9,7 +9,21 @@ import { Button } from "~/components/ui/button";
 
 import { User, MapPin, Calendar1 } from "lucide-react";
 
+import { api } from "~/trpc/server";
+
 // TODO: generate static params for slug
+
+interface Event {
+    description: string;
+    slug: string;
+    id: string;
+    title: string;
+    price: number;
+    capacity: number;
+    hostedAt: string;
+    hostedOn: Date;
+    hostedById: string;
+}
 
 const Event = async ({
     params
@@ -17,7 +31,13 @@ const Event = async ({
     params: Promise<{ slug: string }>
 }) => {
     const eventSlug = (await params).slug;
-    const tags = ["tag 1", "tag 2", "tag 3", "tag 4", "tag 5"];
+    const event = await api.post.getEvent({ slug: eventSlug });
+    
+    // TODO: add loader
+    if (!event)
+        return <></>;
+
+    const tags = await api.post.getTags({ id: event.id });
 
     return (
         <div className="relative mb-[8rem]"> 
@@ -26,25 +46,25 @@ const Event = async ({
                 <div className="absolute inset-0 bg-gradient-to-t from-background to-transparent" />
             </section>
             <section className="fixed lg:hidden z-50 bottom-0 p-2 w-full">
-                <Register mobileView />
+                <Register mobileView event={event} />
             </section>
             <section className="relative flex justify-center mx-6 md:mx-10">
                 <main className="relative flex gap-10 -top-6">
                     <section className="relative max-w-[50rem]">
                         <section>
-                            <h1 className="font-black text-4xl md:text-5xl inline-block text-transparent bg-clip-text bg-gradient-to-r from-[#A330B0] via-[#58138D] to-[#3D017D] dark:from-[#C676C6] dark:via-[#AC79D0] dark:to-[#A261E5]"> Headline </h1>
+                            <h1 className="font-black text-4xl md:text-5xl inline-block text-transparent bg-clip-text bg-gradient-to-r from-[#A330B0] via-[#58138D] to-[#3D017D] dark:from-[#C676C6] dark:via-[#AC79D0] dark:to-[#A261E5]"> {event.title} </h1>
                             <div className="flex flex-wrap lg:w-[30rem] gap-2 mt-2">
-                                {tags.map((tag, idx) => (
-                                    <Tag key={idx} name={tag} />
+                                {tags && tags.map((tag, idx) => (
+                                    <Tag key={idx} name={tag.name} />
                                 ))}
                             </div>
                         </section>
                         <section className="mt-5">
-                            <h2 className="font-extrabold text-xl"> Subhead </h2>
-                            <p className="text-justify"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores labore, libero nulla in ducimus aliquid laboriosam corporis veritatis tenetur corrupti minus quod totam sequi natus cum voluptas nemo ipsum expedita reiciendis explicabo blanditiis possimus provident voluptate. Magni eligendi minus necessitatibus neque ex quia temporibus in cum, voluptatem qui ad perferendis. </p>
+                            <h2 className="font-extrabold text-xl"> Description </h2>
+                            <p className="text-justify"> {event.description} </p>
                         </section>
                         <section className="lg:hidden mt-4">
-                            <VenueInformation mobileView />
+                            <VenueInformation mobileView event={event} />
                         </section>
                         <section className="mt-5">
                             <h2 className="font-extrabold text-xl"> Media </h2>
@@ -81,9 +101,9 @@ const Event = async ({
                             </Carousel>
                         </section>
                     </section>
-                    <section className="min-w-[22rem] hidden lg:flex flex-col gap-[0.1rem] sticky top-8 self-start">
-                        <VenueInformation />
-                        <Register />
+                    <section className="max-w-[22rem] hidden lg:flex flex-col gap-[0.1rem] sticky top-8 self-start">
+                        <VenueInformation event={event} />
+                        <Register event={event} />
                     </section>
                 </main>
             </section>
@@ -99,13 +119,15 @@ const Tag = ({ name }: { name: string }) => {
     );
 }
 
-const Register = ({ mobileView }: { mobileView?: boolean }) => {
+const Register = ({ mobileView, event }: { mobileView?: boolean, event: Event }) => {
+    const price = event.price ? `₱ ${event.price.toFixed(2)}` : "Free";
+
     if (mobileView)
         return (
             <div className="bg-[#F9F9F9] dark:bg-[#261a35] px-5 py-4 rounded-2xl shadow-[0_0_20px_0_rgba(0,0,0,0.4)] dark:shadow-[0_0_30px_0_rgba(0,0,0,0.8)]">
                 <div className="flex justify-between items-end">
-                    <h1 className="font-bold text-[#6A086A] dark:text-[#E4CAFF] text-xl sm:text-2xl"> ₱ 150.00 / Free </h1>
-                    <p className="font-light text-[#474747] dark:text-[#CFCFCF] text-md"> 14 tickets left </p>
+                    <h1 className="font-bold text-[#6A086A] dark:text-[#E4CAFF] text-xl sm:text-2xl">  {price} </h1>
+                    <p className="font-light text-[#474747] dark:text-[#CFCFCF] text-md"> {event.capacity} tickets left </p>
                 </div>
                 <div className="mt-2 bg-black inline-block rounded-md w-full">
                     <Button className="text-md font-bold h-[3rem] sm:h-[3.5rem] w-full bg-gradient-to-r from-[#A330B0] via-[#58138D] to-[#3D017D] dark:from-[#C676C6] dark:via-[#AC79D0] dark:to-[#A261E5] hover:opacity-80 transition-all duration-150"> Register now </Button>
@@ -115,8 +137,8 @@ const Register = ({ mobileView }: { mobileView?: boolean }) => {
     
     return (
         <div className="bg-[#F9F9F9] dark:bg-[#261a35] p-9 rounded-b-xl shadow-xl">
-            <h1 className="font-extrabold text-[#6A086A] dark:text-[#E4CAFF] text-3xl"> ₱ 150.00 / Free </h1>
-            <p className="font-light text-[#474747] dark:text-[#CFCFCF] text-md"> 14 tickets left </p>
+            <h1 className="font-extrabold text-[#6A086A] dark:text-[#E4CAFF] text-3xl"> {price} </h1>
+            <p className="font-light text-[#474747] dark:text-[#CFCFCF] text-md"> {event.capacity} tickets left </p>
             <div className="mt-2 bg-black inline-block rounded-md">
                 <Button className="text-lg font-semibold h-[3rem] w-[10rem] bg-gradient-to-r from-[#A330B0] via-[#58138D] to-[#3D017D] dark:from-[#C676C6] dark:via-[#AC79D0] dark:to-[#A261E5] hover:opacity-80 transition-all duration-150"> Register now </Button>
             </div>
@@ -124,26 +146,46 @@ const Register = ({ mobileView }: { mobileView?: boolean }) => {
     );
 }
 
-const VenueInformation = ({ mobileView }: { mobileView?: boolean }) => {
+const VenueInformation = async ({ mobileView, event }: { mobileView?: boolean, event: Event }) => {
+    const organization = await api.post.getOrganization({ id: event.hostedById });
+    const date = event.hostedOn.toLocaleString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+    });
+    const formattedDate = date.replace('at', ' ·');
+
+    if (!organization)
+        return <></>;
+
     if (mobileView)
         return (
             <div className="bg-[#F9F9F9] dark:bg-[#261a35] p-7 md:p-9 rounded-xl shadow-xl">
                 <h2 className="font-bold"> Venue information </h2>
                 <div className="flex flex-col gap-3 mt-4">
                     <div className="flex items-center gap-4">
-                        <User stroke="#653765" strokeWidth={2} size={24} className="dark:hidden" />
-                        <User stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block" />
-                        <p> FEU Tech ACM Student Chapter </p>
+                        <div className="block">
+                            <User stroke="#653765" strokeWidth={2} size={24} className="dark:hidden" />
+                            <User stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block" />
+                        </div>
+                        <p> {organization.name} </p>
                     </div>
                     <div className="flex items-center gap-4">
-                        <MapPin stroke="#653765" strokeWidth={2} size={24} className="dark:hidden" />
-                        <MapPin stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block" />
-                        <p> FEU Institute of Technology </p>
+                        <div className="block">
+                            <MapPin stroke="#653765" strokeWidth={2} size={24} className="dark:hidden" />
+                            <MapPin stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block" />
+                        </div>
+                        <p> {event.hostedAt} </p>
                     </div>
                     <div className="flex items-center gap-4">
-                        <Calendar1 stroke="#653765" strokeWidth={2} size={24} className="dark:hidden"/>
-                        <Calendar1 stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block"/>
-                        <p> November 10, 2024 · 12:00 PM </p>
+                        <div className="block">
+                            <Calendar1 stroke="#653765" strokeWidth={2} size={24} className="dark:hidden"/>
+                            <Calendar1 stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block"/>
+                        </div>
+                        <p> {formattedDate} </p>
                     </div>
                 </div>
             </div>
@@ -155,19 +197,25 @@ const VenueInformation = ({ mobileView }: { mobileView?: boolean }) => {
                 <h2 className="font-bold"> Venue information </h2>
                 <div className="flex flex-col gap-3 mt-4">
                     <div className="flex items-center gap-4">
-                        <User stroke="#653765" strokeWidth={2} size={24} className="dark:hidden" />
-                        <User stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block" />
-                        <p> FEU Tech ACM Student Chapter </p>
+                        <div className="block">
+                            <User stroke="#653765" strokeWidth={2} size={24} className="dark:hidden" />
+                            <User stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block" />
+                        </div>
+                        <p> {organization.name} </p>
                     </div>
                     <div className="flex items-center gap-4">
-                        <MapPin stroke="#653765" strokeWidth={2} size={24} className="dark:hidden" />
-                        <MapPin stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block" />
-                        <p> FEU Institute of Technology </p>
+                        <div className="block">
+                            <MapPin stroke="#653765" strokeWidth={2} size={24} className="dark:hidden" />
+                            <MapPin stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block" />
+                        </div>
+                        <p> {event.hostedAt} </p>
                     </div>
                     <div className="flex items-center gap-4">
-                        <Calendar1 stroke="#653765" strokeWidth={2} size={24} className="dark:hidden"/>
-                        <Calendar1 stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block"/>
-                        <p> November 10, 2024 · 12:00 PM </p>
+                        <div className="block">
+                            <Calendar1 stroke="#653765" strokeWidth={2} size={24} className="dark:hidden"/>
+                            <Calendar1 stroke="#C676C6" strokeWidth={2} size={24} className="hidden dark:inline-block"/>
+                        </div>
+                        <p> {formattedDate} </p>
                     </div>
                 </div>
             </div>
