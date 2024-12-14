2ecd0cefe4d1f1a11aef6d8c19384d3662abd556
noodles-1 <adrianeroa081202@gmail.com>
2024-12-14 14:43:21 +0800

    refactor: events router

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -15,39 +15,6 @@ export const postRouter = createTRPCRouter({
             };
         }),
 
-    getEvent: publicProcedure
-        .input(z.object({ slug: z.string() }))
-        .query(async ({ ctx, input }) => {
-            return await ctx.db.event.findFirst({
-                where: { slug: input.slug }
-            });
-        }),
-
-    getOrganization: publicProcedure
-        .input(z.object({ id: z.string() }))
-        .query(async ({ ctx, input }) => {
-            return await ctx.db.organization.findFirst({
-                where: { id: input.id }
-            });
-        }),
-
-    getTags: publicProcedure
-        .input(z.object({ id: z.string() }))
-        .query(async ({ ctx, input }) => {
-            const res = await ctx.db.event.findUnique({
-                where: { id: input.id },
-                include: {
-                    tags: {
-                        include: {
-                            tag: true
-                        }
-                    }
-                }
-            });
-            
-            return res?.tags.map(tag => tag.tag)
-        }),
-
     create: protectedProcedure
         .input(z.object({ name: z.string().min(1) }))
         .mutation(async ({ ctx, input }) => {
