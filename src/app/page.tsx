5b2bde2b5970269e2069c73de6c21f3d1702f806
mr-jones123 <xylacap@gmail.com>
2025-01-04 00:25:39 +0800

    feat: add background stars and music

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,18 @@
 import Head from "next/head";
-
+import { ShootingStars } from "~/components/ui/shooting-stars";
+import { StarsBackground } from "~/components/ui/stars-background";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
-      <Head>
-        <title>KPRverse Landing</title>
-        <meta name="description" content="KPRverse-inspired landing page" />
-      </Head>
+      <audio loop autoPlay controls className="z-20" >
+      <source src="/BackgroundMusic.mp3" type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
 
-      {/* Main container */}
-      <div className="space-y-8 px-4 text-center">
-        {/* Simulated Console Window */}
-        <div className="w-full max-w-2xl rounded-lg border border-gray-700 bg-gray-800 p-8 shadow-lg">
-          <p className="font-mono text-lg">Initializing... Loading files...</p>
-          <div className="mt-4">
-            <p className="font-mono text-sm text-gray-400">kai_53815.jpg</p>
-            <p className="font-mono text-sm text-gray-400">
-              audio_log_2018116.wav
-            </p>
-            <p className="font-mono text-sm text-gray-400">
-              activate console for access...
-            </p>
-          </div>
-        </div>
+      <div className="fixed inset-0 z-0 h-full w-full">
 
-        {/* Call to action */}
-        <div className="text-center">
-          <button className="rounded-full bg-indigo-500 px-6 py-3 text-lg font-semibold transition hover:bg-indigo-700">
-            Discover More
-          </button>
-        </div>
+        <StarsBackground />
+        <ShootingStars />
       </div>
     </div>
   );
