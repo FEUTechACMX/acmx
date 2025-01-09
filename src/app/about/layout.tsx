17989de16b609c1c7904bafaeb041b3a8071e653
arrogance231 <alchemyofjhine@gmail.com>
2025-01-09 14:48:46 +0800

    Added the Navigation Bar file to the layout.tsx for the aboutpage so it would stick to all pages under the aboutpage.

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,14 +1,14 @@
-
 import { type Metadata } from "next";
 import "src/styles/2023.css";
 import { TRPCReactProvider } from "~/trpc/react";
-import localFont from 'next/font/local';
+import localFont from "next/font/local";
 import { Habibi } from "next/font/google";
 import { Abhaya_Libre } from "next/font/google";
+import Navbar from "./(components)/navbar";
 
 const bostonAngel = localFont({
-    src : '../../../public/fonts/Boston-Angel.ttf',
-    variable: '--font-BostonAngel',
+  src: "../../../public/fonts/Boston-Angel.ttf",
+  variable: "--font-BostonAngel",
 });
 export const metadata: Metadata = {
   title: "About ACMX",
@@ -22,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${bostonAngel.variable}`}>
       <body>
+        <Navbar />
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
     </html>
