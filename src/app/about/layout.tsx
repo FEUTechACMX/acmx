556af9c41781f09a3a54bf078b394f02afafab8d
arrogance231 <alchemyofjhine@gmail.com>
2025-01-13 21:13:31 +0800

    feat: added  the boston-angel font as a default font under the tailwind-config

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -22,6 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${bostonAngel.variable}`}>
       <body>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Boston+Angel&display=swap"
+          rel="stylesheet"
+        />
         <Navbar />
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
