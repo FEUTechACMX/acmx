31b754018da42f0ee8b8f7f4c27e57437f377a7b
Seedlign <ricbarrios45@gmail.com>
2025-01-13 22:05:19 +0800

    feat: added list for navbar with slight stylings

--- src/app/about/(components)/NavBar.tsx
+++ src/app/about/(components)/NavBar.tsx
@@ -1,5 +1,31 @@
+import { navLinks } from "./index";
+
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
+        <div className="container mx-auto px-4 py-3 flex justify-between items-center bg-[#0B0D17]">
+          <div className="text-xl font-bold text-gray-400 text-center">
+            placeholder
+          </div>
+          <ul className="flex-1 flex justify-center items-center gap-10 max-lg:hidden font-boston-angel font-bold relative">
+            {navLinks.map((item) => (
+              <li key={item.name}>
+                <a
+                  href={item.href}
+                  className="text-white hover:text-gray-700"
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <button className="hidden max-lg:block">Sign In</button>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ src/app/about/(components)/index.js
@@ -0,0 +1,6 @@
+export const navLinks = [
+    { name: "About", href: "/about" },
+    { name: "Featured", href: "/featured" },
+    { name: "Events", href: "/events" },
+    { name: "Committees", href: "/committees" },
+  ];
