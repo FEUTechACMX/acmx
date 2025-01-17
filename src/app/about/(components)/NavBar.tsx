a150ea72be327dab74da85eae1661a636ecbf40a
Xynil Jhed Lacap <lacapxyniljhed@gmail.com>
2025-01-17 16:18:28 +0800

    fix: overflow in main page and fixed navbar

--- src/app/about/(components)/NavBar.tsx
+++ src/app/about/(components)/NavBar.tsx
@@ -2,11 +2,10 @@ import { navLinks } from "./index";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
-        <div className="container mx-auto flex items-center justify-between bg-[#0B0D17] px-4 py-3">
-          <div className="text-center text-xl font-bold text-gray-400">
-            placeholder
+      <nav className="bg-[#0B0D17] sticky top-0">
+        <div className="container mx-auto flex items-center justify-between  px-4 py-3">
+          <div className="text-center text-xl font-bold text-customWhite">
+            ACMX
           </div>
           <ul className="relative flex flex-1 items-center justify-center gap-10 font-header font-bold max-lg:hidden">
             {navLinks.map((item) => (
@@ -20,7 +19,6 @@ const Navbar = () => {
           <button className="hidden max-lg:block">Sign In</button>
         </div>
       </nav>
-    </div>
   );
 };
 
