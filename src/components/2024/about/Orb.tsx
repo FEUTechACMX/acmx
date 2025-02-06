cf7cc07f345fcf624e97f21dae181a7eb4a93bdf
OntheBarProg <202311340@fit.edu.ph>
2025-02-07 06:00:37 +0800

    Feat: Positioned the star component and added more rings

--- src/components/2024/about/Orb.tsx
+++ src/components/2024/about/Orb.tsx
@@ -1,23 +1,46 @@
 import Image from "next/image";
 
+
+
+
 const Orb = () => {
   return (
-    <div className="relative flex h-[200px] w-[200px] items-center justify-center">
+    <div className=" relative flex h-[400px] w-[400px] items-center justify-center">
       <div className="absolute">
         <Image
           src="/about/orb/Orb-min.png"
           alt="orb"
-          width={100}
-          height={100}
+          width={125}
+          height={125}
         />
       </div>
       <div className="absolute">
         <Image
           src="/about/orb/Ring1-min.png"
           alt="ring"
-          width={200}
-          height={200}
+          width={150}
+          height={150}
           className="animate-spin"
+        />
+      </div>
+      <div className="absolute">
+        <Image
+          src="/about/orb/Ring2-min.png"
+          alt="ring"
+          width={225}
+          height={225}
+          className="animate-spin"
+
+        />
+      </div>
+      <div className="absolute">
+        <Image
+          src="/about/orb/Ring3-min.png"
+          alt="ring"
+          width={275}
+          height={275}
+          className="animate-spin"
+
         />
       </div>
     </div>
